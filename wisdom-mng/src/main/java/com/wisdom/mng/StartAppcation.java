eb5aa2c3c6a77891b7b77ecbd3c0f09b8378a112
chenweicong <654129588@qq.com>
2019-01-22 15:06:36 +0800

    2019.01.22

--- wisdom-mng/src/main/java/com/wisdom/mng/StartAppcation.java
+++ wisdom-mng/src/main/java/com/wisdom/mng/StartAppcation.java
@@ -1,10 +1,18 @@
 package com.wisdom.mng;
 
 import com.wisdom.mng.support.CustomRepositoryFactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.MultipartConfigElement;
 
 /**
  * 启动类
@@ -12,8 +20,33 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @ComponentScan(basePackages={"com.wisdom.mng"})
 @EnableJpaRepositories(repositoryFactoryBeanClass = CustomRepositoryFactoryBean.class)
-public class StartAppcation
-{
+public class StartAppcation{
+
+    @Autowired
+    private Environment env;
+
+    //跨域设置，允许携带cookie
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedHeaders("*")
+                        .allowedMethods("*")
+                        .allowedOrigins("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+    @Bean
+    MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setLocation(env.getProperty("upload.file.basedir"));
+        return factory.createMultipartConfig();
+    }
+
     public static void main( String[] args )
     {
         SpringApplication.run(StartAppcation.class,args);
